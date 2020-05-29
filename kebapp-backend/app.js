161ffd4ef3f7034ae15926f09e4ba89776736514
Szajkovics Sándor <37326703+KEBABSELLER6@users.noreply.github.com>
2020-05-29 10:34:36 +0200

    Defined enpoints

--- kebapp-backend/app.js
+++ kebapp-backend/app.js
@@ -20,3 +20,15 @@ app.get('/', (req,res)=>{
         "message": "Hello world!"
     });
 })
+
+app.post('/rate', (req,res)=>{
+    //req inculedes the unique restaurant name, the price and the rating type: either positive or negative
+    //res sends back the new rating for the price
+    //update the row in the collection
+})
+
+app.get('/restaurant', (req,res)=>{
+    //req includes the current longitude, latitude
+    //res sends back the restaurants and the prices for the local area
+    //query from the collection, and respond with a list
+})
