f94adae912bcd1b44c3298ca607a2e044e6869e9
Szajkovics Sándor <37326703+KEBABSELLER6@users.noreply.github.com>
2020-05-29 09:56:22 +0200

    Database connection implementation

--- kebapp-backend/app.js
+++ kebapp-backend/app.js
@@ -1,11 +1,19 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.listen(3000, ()=>{
+    connectToDB();
     console.log('Listening on http://localhost:3000/');
 });
+
+function connectToDB(){
+    mongoose.connect('mongodb://localhost/kebapp',{useUnifiedTopology: true, useNewUrlParser: true},()=>{
+        console.log("connected to db");
+    });
+}
 
 app.get('/', (req,res)=>{
     res.json({
