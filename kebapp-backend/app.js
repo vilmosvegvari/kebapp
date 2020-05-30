9989d6015f8e2286860933ae279f9f99125a1bf3
Szajkovics Sándor <37326703+KEBABSELLER6@users.noreply.github.com>
2020-05-30 10:24:45 +0200

    Updated endpoints

--- kebapp-backend/app.js
+++ kebapp-backend/app.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const https = require('https');
 
 const cfg = require('./config');
 const app = express();
 const port = cfg.port;
 const mongoIP = cfg.mongoIP;
+const mapboxApiKey = cfg.mapboxApiKey;
 
 app.listen(port, ()=>{
     connectToDB();
@@ -28,10 +30,31 @@ app.post('/rate', (req,res)=>{
     //req inculedes the unique restaurant name, the price and the rating type: either positive or negative
     //res sends back the new rating for the price
     //update the row in the collection
+    //??could go on the /price endpoint with post
+})
+
+app.get('/price', (req,res)=>{
+    //req includes the coordinates for the place
+    //res sends back prices for the location
+    //query from our mongodb
 })
 
 app.get('/restaurant', (req,res)=>{
-    //req includes the current longitude, latitude
-    //res sends back the restaurants and the prices for the local area
-    //query from the collection, and respond with a list
+    //req includes the bbox coordinates
+    //eg: 
+    /*
+        {
+            "left corner": {
+                "x": 77.083056,
+                "y": 38.908611
+            },
+            "right corner": {
+                "x": -76.997778,
+                "y": 38.959167
+            }
+        }
+    */
+   //res sends back the result from the query using mapbox places
+   //query from https://api.mapbox.com/geocoding/v5/mapbox.places/ and send the data to the frontend
+   //https query eg: https://api.mapbox.com/geocoding/v5/mapbox.places/kebab.json?bbox=-77.083056,38.908611,-76.997778,38.959167&access_token=mapboxApiKey
 })
--- kebapp-backend/config.js
+++ kebapp-backend/config.js
@@ -3,5 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   port: process.env.PORT,
-  mongoIP: process.env.MONGO_IP
+  mongoIP: process.env.MONGO_IP,
+  mapboxApiKey: process.env.MAPBOX_API_KEY
 };
