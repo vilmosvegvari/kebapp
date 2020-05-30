2cf10f50b0f24353021dc4d0257fecfd8a9c72fa
vilmosvegvari <vegvari.vili@gmail.com>
2020-05-30 14:27:00 +0200

    Frontend shows a map
    Mapbox added.

--- kebapp-frontend/src/index.js
+++ kebapp-frontend/src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 16.83519,
+      lat: 47.556339,
+      zoom: 13,
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/outdoors-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
